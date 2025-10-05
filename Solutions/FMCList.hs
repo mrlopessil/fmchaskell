b0804038acd8649ac08a9ae8bfb0484f1aff2e1c
Maria Rita Lopes Silva <mr.lopessil@gmail.com>
2025-10-05 18:22:43 -0300

    defined the zips functions

--- Solutions/FMCList.hs
+++ Solutions/FMCList.hs
@@ -232,8 +232,15 @@ isInfixOf xs ys = any (isPrefixOf xs) (tails ys)
 isSuffixOf :: Eq a => [a] -> [a] -> Bool
 isSuffixOf xs ys = reverse xs `isPrefixOf` reverse ys
 
--- zip
--- zipWith
+zip :: [a] -> [b] -> [(a, b)]
+zip [] _ = []
+zip _ [] = []
+zip (x:xs) (y:ys) = (x,y) : zip xs ys
+
+zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith _ [] _ = []
+zipWith _ _ [] = []
+zipWith f (x:xs) (y:ys) = f x y : zipWith f xs ys
 
 -- intercalate
 -- nub
